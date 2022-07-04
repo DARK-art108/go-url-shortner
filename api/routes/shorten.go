e978edcc28bb48302b2bf825128fb9830ad51b33
Ritesh <daydreamingguy941@gmail.com>
2022-07-04 22:06:15 +0530

    Added helpers functions

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,7 +1,10 @@
 package routes
 
 import (
+	"fiber"
 	"time"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 type request struct {
@@ -16,4 +19,23 @@ type response struct {
 	Expiry          time.Duration `json:"expiry"`
 	XRateRemaining  int           `json:"rate_limit"`
 	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse json"})
+	}
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Domain Error"})
+	}
+
+	body.URL = helpers.EnforceHTTP(body.URL)
 }
